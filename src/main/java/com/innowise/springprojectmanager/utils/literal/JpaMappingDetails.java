841d32d6d15e4803115e4910ab1d1708d915d37a
Edvard Krainiy <ekrayniy@inbox.ru>
2022-04-03 15:40:05 +0300

    Added tasks table and task entity.

--- src/main/java/com/innowise/springprojectmanager/utils/literal/JpaMappingDetails.java
+++ src/main/java/com/innowise/springprojectmanager/utils/literal/JpaMappingDetails.java
@@ -7,7 +7,9 @@ package com.innowise.springprojectmanager.utils.literal;
  */
 public class JpaMappingDetails {
   public static final String USERS_TABLE = "users";
+  public static final String TASKS_TABLE = "tasks";
   public static final String USER_ID = "user_id";
+  public static final String PROJECT_ID = "user_id";
   public static final String PROJECTS_TABLE = "projects";
   public static final String ID = "id";
   public static final String USERNAME = "username";
@@ -17,10 +19,12 @@ public class JpaMappingDetails {
   public static final String ROLE = "role";
   public static final String ACTIVATED = "activated";
   public static final String USER = "user";
+  public static final String PROJECT = "project";
   public static final String ISSUED_AT = "issued_at";
-  public static final String PROJECT_TITLE = "title";
-  public static final String PROJECT_DESCRIPTION = "description";
-  public static final String IS_DELETED = "deleted";
+  public static final String EXPIRES_AT = "expires_at";
+  public static final String TITLE = "title";
+  public static final String DESCRIPTION = "description";
+  public static final String DELETED = "deleted";
 
   private JpaMappingDetails() {}
 }
